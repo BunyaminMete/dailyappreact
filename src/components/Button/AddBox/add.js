8989cb07585a7eaafcf9651e21faa9c49ec4bd9a
Bunyamin Mete <kaanmetee5@gmail.com>
2022-08-02 15:46:47 +0300

    commit -m

--- src/components/Button/AddBox/add.js
+++ src/components/Button/AddBox/add.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 export default function AddButton(props) {
   // eslint-disable-next-line react/prop-types
   const { addElement } = props; //props küçük harfle
